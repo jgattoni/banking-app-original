9c21b2d282cec00ecb91cb628cdccf81d08e5628
Rowingbobcat <julien.gattoni@gmail.com>
2025-07-01 23:19:52 +0200

    implement full layout

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-    const loggedIn = { firstName: 'Julien' };
-  return (
-    <section className="home">
-      <div className="home-content">
-        <header className="home-header">
-          <HeaderBox
-              type="greeting"
-              title="Welcome"
-              user={loggedIn.firstName || 'Guest'}
-              subtext={"Access and manage your account and transactions efficiently."}
-          />
-        <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
-        />
-        </header>
-      </div>
-    </section>
-  )
+    const loggedIn = { firstName: 'Julien', lastName: 'Gattoni', email: 'julien.gattoni@gmail.com' }
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                      type="greeting"
+                      title="Welcome"
+                      user={loggedIn.firstName || 'Guest'}
+                      subtext={"Access and manage your account and transactions efficiently."}
+                    />
+                    <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                    />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50},{currentBalance: 500.00}]}
+            />
+        </section>
+    )
 }
 
 export default Home
